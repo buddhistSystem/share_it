6e5c4dd3ab279059aaede73a579e98f517c973b3
liqian <lq1083301982@163.com>
2021-01-02 16:29:33 +0800

    集成nacos，上传nacos-server软件

--- content-center/src/main/java/com/demo/contnetcenter/service/share/ShareService.java
+++ content-center/src/main/java/com/demo/contnetcenter/service/share/ShareService.java
@@ -5,10 +5,15 @@ import com.demo.contnetcenter.domain.dto.content.ShareDto;
 import com.demo.contnetcenter.domain.dto.user.UserDto;
 import com.demo.contnetcenter.domain.entity.Share;
 import org.springframework.beans.BeanUtils;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 import javax.annotation.Resource;
+import java.util.List;
+import java.util.concurrent.ThreadLocalRandom;
+import java.util.stream.Collectors;
 
 @Service
 public class ShareService {
@@ -16,16 +21,29 @@ public class ShareService {
     @Resource
     private ShareMapper shareMapper;
 
+    @Resource
+    private DiscoveryClient discoveryClient;
+
     public ShareDto findById(Integer id) {
         //获取分享详情
         Share share = shareMapper.selectByPrimaryKey(id);
         //通过share中的用户id获取用户信息
         Integer userId = share.getId();
-        //调用用user-center服务
+        //使用http调用用user-center服务,传统方式url是写死的，微服务能动态获取
+        List<ServiceInstance> instances = discoveryClient.getInstances("user-center");
+        //客户端负载均衡
+        List<String> urls = instances.stream()
+                .map(instance -> instance.getUri() + "/users/{id}")
+                .collect(Collectors.toList());
+        int random = ThreadLocalRandom.current().nextInt(urls.size());
+        String url = urls.get(random);
+        System.out.println(url);
+
         RestTemplate restTemplate = new RestTemplate();
-        UserDto userDto = restTemplate.getForObject("http://localhost:8080/users/{id}", UserDto.class, userId);
+        UserDto userDto = restTemplate.getForObject(url, UserDto.class, userId);
+
         ShareDto shareDto = new ShareDto();
-        BeanUtils.copyProperties(share,shareDto);
+        BeanUtils.copyProperties(share, shareDto);
         shareDto.setWxNickname(userDto.getWxNickname());
         return shareDto;
     }
--- content-center/src/test/java/com/demo/contnetcenter/UserCenterApplicationTests.java
+++ content-center/src/test/java/com/demo/contnetcenter/UserCenterApplicationTests.java
@@ -1,15 +1,41 @@
 package com.demo.contnetcenter;
 
 
+import com.alibaba.fastjson.JSON;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import javax.annotation.Resource;
+import java.util.List;
 
 @SpringBootTest
-class UserCenterApplicationTests {
+@RunWith(SpringJUnit4ClassRunner.class)
+public class UserCenterApplicationTests {
 
     @Test
-    public void test(){
+    public void test() {
 
     }
+
+    @Resource
+    private DiscoveryClient discoveryClient;
+
+    /**
+     * 测试发现服务
+     */
+    @Test
+    public void test1() {
+        //查询所有user-center的服务信息
+        List<ServiceInstance> instances = discoveryClient.getInstances("user-center");
+        System.out.println(JSON.toJSON(instances));
+        List<String> services = discoveryClient.getServices();
+        System.out.println(services);
+    }
+
 
 }

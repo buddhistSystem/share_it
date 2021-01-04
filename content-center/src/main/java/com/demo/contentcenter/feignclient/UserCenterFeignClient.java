9e06d82a8fd50fd162f3cd60fe7a981a4e62c3e3
liqian <lq1083301982@163.com>
2021-01-04 16:29:35 +0800

    feign 性能优化

--- content-center/src/main/java/com/demo/contentcenter/feignclient/UserCenterFeignClient.java
+++ content-center/src/main/java/com/demo/contentcenter/feignclient/UserCenterFeignClient.java
@@ -1,6 +1,5 @@
 package com.demo.contentcenter.feignclient;
 
-import com.demo.contentcenter.config.UserCenterFeignConfig;
 import com.demo.contentcenter.domain.dto.user.UserDto;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,8 +7,10 @@ import org.springframework.web.bind.annotation.PathVariable;
 
 /**
  * user-center服务feign客户端
+ * configuration可以指定单个feignClient的自定义配置，目前采用全局配置，暂时弃用
  */
-@FeignClient(name = "user-center",configuration = UserCenterFeignConfig.class)
+@FeignClient(name = "user-center")
+//@FeignClient(name = "user-center",configuration = UserCenterFeignConfig.class)
 public interface UserCenterFeignClient {
 
     @GetMapping("/users/{id}")

91568a8391736520d4151692f63e519c26d76d9f
liqian <lq1083301982@163.com>
2021-01-12 11:09:32 +0800

    sentinel区分来源

---
+++ content-center/src/main/java/com/demo/contentcenter/sentinel/CustomizeSentinelRequestOriginParser.java
@@ -0,0 +1,26 @@
+package com.demo.contentcenter.sentinel;
+
+import com.alibaba.csp.sentinel.adapter.spring.webmvc.callback.RequestOriginParser;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * sentinel 区分来源
+ * <p>
+ * 流控规则，授权规则种可以指定来源
+ */
+//@Component
+public class CustomizeSentinelRequestOriginParser implements RequestOriginParser {
+
+    @Override
+    public String parseOrigin(HttpServletRequest request) {
+        // 从参数中获取origin,不推荐该种方式，可以将origin写入httpHeader中
+        String origin = request.getParameter("origin");
+        if (StringUtils.isBlank(origin)) {
+            throw new IllegalArgumentException("origin 来源必须被指定！");
+        }
+        return origin;
+    }
+}

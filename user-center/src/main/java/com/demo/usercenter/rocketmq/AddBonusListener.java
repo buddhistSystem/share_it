c1b75d076d7709319e499ea0e8a9762d9efa30ae
liqian <lq1083301982@163.com>
2021-01-16 10:48:36 +0800

    重构spring编程模型mq消费者代码

--- user-center/src/main/java/com/demo/usercenter/rocketmq/AddBonusListener.java
+++ user-center/src/main/java/com/demo/usercenter/rocketmq/AddBonusListener.java
@@ -1,17 +1,13 @@
 package com.demo.usercenter.rocketmq;
 
-import com.demo.usercenter.dao.BonusEventLogMapper;
-import com.demo.usercenter.dao.user.UserMapper;
 import com.demo.usercenter.domain.dto.messaging.UserAddBonusMsgDto;
-import com.demo.usercenter.domain.entity.BonusEventLog;
-import com.demo.usercenter.domain.entity.User;
+import com.demo.usercenter.service.user.UserService;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.rocketmq.spring.annotation.RocketMQMessageListener;
 import org.apache.rocketmq.spring.core.RocketMQListener;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
-import java.util.Date;
 
 /**
  * 间挺add-bonus主题
@@ -22,27 +18,11 @@ import java.util.Date;
 public class AddBonusListener implements RocketMQListener<UserAddBonusMsgDto> {
 
     @Resource
-    private UserMapper userMapper;
-
-    @Resource
-    private BonusEventLogMapper bonusEventLogMapper;
+    private UserService userService;
 
     @Override
 
     public void onMessage(UserAddBonusMsgDto userAddBonusMsgDto) {
-        //1.为用户增加积分
-        Integer userId = userAddBonusMsgDto.getUserId();
-        User user = this.userMapper.selectByPrimaryKey(userId);
-        Integer bonus = userAddBonusMsgDto.getBonus();
-        user.setBonus(bonus);
-        this.userMapper.updateByPrimaryKey(user);
-        //2.记录日志到bonus_event_log表中
-        this.bonusEventLogMapper.insertSelective(BonusEventLog.builder()
-                .userId(userId)
-                .value(bonus)
-                .event("CONTRIBUTE")
-                .createTime(new Date())
-                .description("投稿加积分")
-                .build());
+        this.userService.receive(userAddBonusMsgDto);
     }
 }
--- user-center/src/main/java/com/demo/usercenter/service/user/UserService.java
+++ user-center/src/main/java/com/demo/usercenter/service/user/UserService.java
@@ -1,10 +1,15 @@
 package com.demo.usercenter.service.user;
 
+import com.demo.usercenter.dao.BonusEventLogMapper;
 import com.demo.usercenter.dao.user.UserMapper;
+import com.demo.usercenter.domain.dto.messaging.UserAddBonusMsgDto;
+import com.demo.usercenter.domain.entity.BonusEventLog;
 import com.demo.usercenter.domain.entity.User;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import javax.annotation.Resource;
+import java.util.Date;
 
 @Service
 public class UserService {
@@ -12,7 +17,28 @@ public class UserService {
     @Resource
     private UserMapper userMapper;
 
+    @Resource
+    private BonusEventLogMapper bonusEventLogMapper;
+
     public User findById(Integer id) {
         return userMapper.selectByPrimaryKey(id);
+    }
+
+    @Transactional(rollbackFor = Exception.class)
+    public void receive(UserAddBonusMsgDto userAddBonusMsgDto) {
+        //1.为用户增加积分
+        Integer userId = userAddBonusMsgDto.getUserId();
+        User user = this.userMapper.selectByPrimaryKey(userId);
+        Integer bonus = userAddBonusMsgDto.getBonus();
+        user.setBonus(bonus);
+        this.userMapper.updateByPrimaryKey(user);
+        //2.记录日志到bonus_event_log表中
+        this.bonusEventLogMapper.insertSelective(BonusEventLog.builder()
+                .userId(userId)
+                .value(bonus)
+                .event("CONTRIBUTE")
+                .createTime(new Date())
+                .description("投稿加积分")
+                .build());
     }
 }

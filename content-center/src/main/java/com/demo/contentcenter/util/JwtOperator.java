753f941052104e49324cf2e7264d0de26eac3554
liqian <lq1083301982@163.com>
2021-01-17 12:52:11 +0800

    user-center,content-center 集成jjwt

---
+++ content-center/src/main/java/com/demo/contentcenter/util/JwtOperator.java
@@ -0,0 +1,160 @@
+package com.demo.contentcenter.util;
+
+import com.alibaba.nacos.client.identify.Base64;
+import com.google.common.collect.Maps;
+import com.netflix.client.config.IClientConfigKey;
+import io.jsonwebtoken.*;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+
+@Slf4j
+@RequiredArgsConstructor
+@SuppressWarnings("WeakerAccess")
+@Component
+public class JwtOperator {
+    /**
+     * 秘钥
+     * - 默认aabbccddee
+     */
+    @Value("${secret:aabbccddee}")
+    private String secret;
+    /**
+     * 有效期，单位秒
+     * - 默认2周
+     */
+    @Value("${expire-time-in-second:1209600}")
+    private Long expirationTimeInSecond;
+
+    /**
+     * 从token中获取claim
+     *
+     * @param token token
+     * @return claim
+     */
+    public Claims getClaimsFromToken(String token) {
+        try {
+            return Jwts.parser()
+                    .setSigningKey(this.secret.getBytes())
+                    .parseClaimsJws(token)
+                    .getBody();
+        } catch (ExpiredJwtException | UnsupportedJwtException | MalformedJwtException | IllegalArgumentException e) {
+            log.error("token解析错误", e);
+            throw new IllegalArgumentException("Token invalided.");
+        }
+    }
+
+    /**
+     * 获取token的过期时间
+     *
+     * @param token token
+     * @return 过期时间
+     */
+    public Date getExpirationDateFromToken(String token) {
+        return getClaimsFromToken(token)
+                .getExpiration();
+    }
+
+    /**
+     * 判断token是否过期
+     *
+     * @param token token
+     * @return 已过期返回true，未过期返回false
+     */
+    private Boolean isTokenExpired(String token) {
+        Date expiration = getExpirationDateFromToken(token);
+        return expiration.before(new Date());
+    }
+
+    /**
+     * 计算token的过期时间
+     *
+     * @return 过期时间
+     */
+    private Date getExpirationTime() {
+        return new Date(System.currentTimeMillis() + this.expirationTimeInSecond * 1000);
+    }
+
+    /**
+     * 为指定用户生成token
+     *
+     * @param claims 用户信息
+     * @return token
+     */
+    public String generateToken(Map<String, Object> claims) {
+        Date createdTime = new Date();
+        Date expirationTime = this.getExpirationTime();
+
+
+        byte[] keyBytes = secret.getBytes();
+        SecretKey key = new SecretKeySpec(keyBytes, 0, keyBytes.length, "AES");
+
+        return Jwts.builder()
+                .setClaims(claims)
+                .setIssuedAt(createdTime)
+                .setExpiration(expirationTime)
+                // 你也可以改用你喜欢的算法
+                // 支持的算法详见：https://github.com/jwtk/jjwt#features
+                .signWith(SignatureAlgorithm.HS256, key)
+                .compact();
+    }
+
+    /**
+     * 判断token是否非法
+     *
+     * @param token token
+     * @return 未过期返回true，否则返回false
+     */
+    public Boolean validateToken(String token) {
+        return !isTokenExpired(token);
+    }
+
+    public static void main(String[] args) {
+        // 1. 初始化
+        JwtOperator jwtOperator = new JwtOperator();
+        jwtOperator.expirationTimeInSecond = 1209600L;
+        jwtOperator.secret = "aaabbbcccdddeeefffggghhhiiijjjkkklllmmmnnnooopppqqqrrrsssttt";
+
+        // 2.设置用户信息
+        HashMap<String, Object> objectObjectHashMap = Maps.newHashMap();
+        objectObjectHashMap.put("id", "1");
+
+        // 测试1: 生成token
+        String token = jwtOperator.generateToken(objectObjectHashMap);
+        System.out.println(token);
+
+        // 将我改成上面生成的token!!!
+        String someToken = "eyJhbGciOiJIUzI1NiJ9.eyJpZCI6IjEiLCJleHAiOjE2MTIwNjgxNTAsImlhdCI6MTYxMDg1ODU1MH0.KHrA_J3Cv2lTp-8HTMhNl1f1_sEpMEjee8E-V7CQCxs";
+        // 测试2: 如果能token合法且未过期，返回true
+        Boolean validateToken = jwtOperator.validateToken(someToken);
+        System.out.println(validateToken);
+
+        // 测试3: 获取用户信息
+        Claims claims = jwtOperator.getClaimsFromToken(someToken);
+        System.out.println(claims);
+
+        // 将我改成你生成的token的第一段（以.为边界）
+        String encodedHeader = "eyJhbGciOiJIUzI1NiJ9";
+        // 测试4: 解密Header
+        byte[] header = Base64.decodeBase64(encodedHeader.getBytes());
+        System.out.println(new String(header));
+
+        // 将我改成你生成的token的第二段（以.为边界）
+        String encodedPayload = "eyJpZCI6IjEiLCJleHAiOjE2MTIwNjgxNTAsImlhdCI6MTYxMDg1ODU1MH0";
+        // 测试5: 解密Payload
+        byte[] payload = Base64.decodeBase64(encodedPayload.getBytes());
+        System.out.println(new String(payload));
+
+        // 测试6: 这是一个被篡改的token，因此会报异常，说明JWT是安全的
+        jwtOperator.validateToken("eyJhbGciOiJIUzI1NiJ9.eyJpZCI6IjEiLCJleHAiOjE2MTIwNjgxNTAsImlhdCI6MTYxMDg1ODU1MH0.KHrA_J3Cv2lTp-8HTMhNl1f1_sEpMEjee8E-V7CQCxs");
+    }
+
+}
---
+++ user-center/src/main/java/com/demo/usercenter/util/JwtOperator.java
@@ -0,0 +1,113 @@
+package com.demo.usercenter.util;
+
+import io.jsonwebtoken.*;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
+import java.util.Date;
+import java.util.Map;
+
+
+@Slf4j
+@RequiredArgsConstructor
+@SuppressWarnings("WeakerAccess")
+@Component
+public class JwtOperator {
+    /**
+     * 秘钥
+     * - 默认aabbccddee
+     */
+    @Value("${secret:aabbccddee}")
+    private String secret;
+    /**
+     * 有效期，单位秒
+     * - 默认2周
+     */
+    @Value("${expire-time-in-second:1209600}")
+    private Long expirationTimeInSecond;
+
+    /**
+     * 从token中获取claim
+     *
+     * @param token token
+     * @return claim
+     */
+    public Claims getClaimsFromToken(String token) {
+        try {
+            return Jwts.parser()
+                    .setSigningKey(this.secret.getBytes())
+                    .parseClaimsJws(token)
+                    .getBody();
+        } catch (ExpiredJwtException | UnsupportedJwtException | MalformedJwtException | IllegalArgumentException e) {
+            log.error("token解析错误", e);
+            throw new IllegalArgumentException("Token invalided.");
+        }
+    }
+
+    /**
+     * 获取token的过期时间
+     *
+     * @param token token
+     * @return 过期时间
+     */
+    public Date getExpirationDateFromToken(String token) {
+        return getClaimsFromToken(token)
+                .getExpiration();
+    }
+
+    /**
+     * 判断token是否过期
+     *
+     * @param token token
+     * @return 已过期返回true，未过期返回false
+     */
+    private Boolean isTokenExpired(String token) {
+        Date expiration = getExpirationDateFromToken(token);
+        return expiration.before(new Date());
+    }
+
+    /**
+     * 计算token的过期时间
+     *
+     * @return 过期时间
+     */
+    private Date getExpirationTime() {
+        return new Date(System.currentTimeMillis() + this.expirationTimeInSecond * 1000);
+    }
+
+    /**
+     * 为指定用户生成token
+     *
+     * @param claims 用户信息
+     * @return token
+     */
+    public String generateToken(Map<String, Object> claims) {
+        Date createdTime = new Date();
+        Date expirationTime = this.getExpirationTime();
+
+
+        byte[] keyBytes = secret.getBytes();
+        SecretKey key = new SecretKeySpec(keyBytes, 0, keyBytes.length, "AES");
+
+        return Jwts.builder()
+                .setClaims(claims)
+                .setIssuedAt(createdTime)
+                .setExpiration(expirationTime)
+                .signWith(SignatureAlgorithm.HS256, key)
+                .compact();
+    }
+
+    /**
+     * 判断token是否非法
+     *
+     * @param token token
+     * @return 未过期返回true，否则返回false
+     */
+    public Boolean validateToken(String token) {
+        return !isTokenExpired(token);
+    }
+}

d2a457e4f3d5a1fe3662ca8bd27e8c41bd474ddc
liqian <lq1083301982@163.com>
2021-01-20 12:13:36 +0800

    在当前类中添加@RefreshScope注解，即可动态刷新该类中的配置

--- content-center/src/main/java/com/demo/contentcenter/ContentCenterApplication.java
+++ content-center/src/main/java/com/demo/contentcenter/ContentCenterApplication.java
@@ -3,10 +3,10 @@ package com.demo.contentcenter;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.cloud.stream.annotation.EnableBinding;
 import org.springframework.cloud.stream.messaging.Source;
-import org.springframework.messaging.support.MessageBuilder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import tk.mybatis.spring.annotation.MapperScan;
@@ -16,6 +16,7 @@ import tk.mybatis.spring.annotation.MapperScan;
 @SpringBootApplication
 @RestController
 @EnableBinding(Source.class)
+@RefreshScope
 public class ContentCenterApplication {
 
     public static void main(String[] args) {

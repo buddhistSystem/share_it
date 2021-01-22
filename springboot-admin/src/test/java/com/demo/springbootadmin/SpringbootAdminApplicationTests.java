768dd143267b53969655d56b93e0cec97cc33087
liqian <lq1083301982@163.com>
2021-01-22 17:54:46 +0800

    新增springboot-admin监控

---
+++ springboot-admin/src/test/java/com/demo/springbootadmin/SpringbootAdminApplicationTests.java
@@ -0,0 +1,14 @@
+package com.demo.springbootadmin;
+
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootAdminApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
